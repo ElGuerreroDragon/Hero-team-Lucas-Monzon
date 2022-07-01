cd997ee80c3c14e53fbb9cd0730ffa25b4ae23a6
lmonzon25 <lmonzon@deplm.com.ar>
2022-07-01 15:53:52 -0300

    cambiar espacio por guiones
    cambiar espacios por guiones

--- main.js
+++ main.js
@@ -9,16 +9,27 @@ const personajes = {
       .then((res) => res.json())
       .then((json) => {
         for (const heroe of json) {
-          let nombre = heroe.biography.fullName;
-          contentHTML += `
-      <div class="col-md-2 text-center">
-        <a  target="_blank" id="nombrePerso">
-          <img src="${heroe.images.lg}" alt="${heroe.name}" class="img-thumbnail">
-        </a>
-        <h3 class="title">${heroe.name}</h3>
-      </div>`;
-        
-        console.log(nombre)
+          let nombre = heroe.name;
+          let espacio=" ";
+          if (nombre.indexOf(" ") === -1){
+            contentHTML += `
+            <div class="col-md-2 text-center">
+              <a onclick=datosChamp("${nombre}") target="_blank" class="nombrePerso">
+                <img src="${heroe.images.lg}" alt="${heroe.name}" class="img-thumbnail">
+              </a>
+              <h3 class="title">${heroe.name}</h3>
+            </div>`;
+          } else{
+
+            nombre = nombre.replace(" ","-")
+            contentHTML += `
+            <div class="col-md-2 text-center">
+              <a onclick=datosChamp("${nombre}") target="_blank" class="nombrePerso">
+                <img src="${heroe.images.lg}" alt="${heroe.name}" class="img-thumbnail">
+              </a>
+              <h3 class="title">${heroe.name}</h3>
+            </div>`;
+          }
         }
         container.innerHTML = contentHTML;
       });
@@ -28,3 +39,6 @@ const personajes = {
 
 personajes.render();
 
+function datosChamp(a){
+  alert(a)
+}
