0d39abbac2d749a89e96a4f7a14f9f3821b05bf4
lmonzon25 <lmonzon@deplm.com.ar>
2022-07-04 15:39:04 -0300

    pagina de estadisticas y contacto
    pagina de estadisticas y contacto

--- stats.js
+++ stats.js
@@ -10,16 +10,20 @@ const champs = {
       .then((json) => {
         for(heroe of json){
         contentHTML += `
-        <div class="col-md-2 text-center">
-          <a onclick=datosChamp("") target="_blank" class="nombrePerso">
-            <img src="${heroe.images.lg}" alt="${heroe.name}" class="img-thumbnail">
-          </a>
-          <h3 class="title">${heroe.name}</h3>
-        </div>`;
+        <tr class="img-thumbnail">
+        <td> <img src="${heroe.images.sm}" alt="${heroe.name}" heigth="10px"></td>
+        <td>${heroe.id}</td>
+        <td>${heroe.name}</td>
+        <td>${heroe.powerstats.intelligence}%</td>
+        <td>${heroe.powerstats.strength}%</td>
+        <td>${heroe.powerstats.speed}%</td>
+        <td>${heroe.powerstats.durability}%</td>
+        <td>${heroe.powerstats.power}%</td>
+        <td>${heroe.powerstats.combat}%</td>
+        <th>`
       }
         container.innerHTML = contentHTML;
-      });
-      
+      });     
   },
 };
 
@@ -28,3 +32,10 @@ champs.render();
 function datosChamp(a){
   alert(a)
 }
+
+//<div class="col-md-12 text-center">
+//<a onclick=datosChamp("") target="_blank" class="nombrePerso">
+//</div>  <img src="${heroe.images.lg}" alt="${heroe.name}" width="100" height="40" class="img-thumbnail">
+//</img></a>
+//<h3 class="title">${heroe.name}</h3>
+//</div>`;
